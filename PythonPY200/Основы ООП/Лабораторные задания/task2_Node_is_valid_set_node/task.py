263a3ac6e32572b37f6171b197042f46469be4e1
dmierm <z11grun@gmail.com>
2021-09-02 21:13:55 +0300

    init

---
+++ PythonPY200/Основы ООП/Лабораторные задания/task2_Node_is_valid_set_node/task.py
@@ -0,0 +1,30 @@
+from typing import Any, Optional
+
+
+class Node:
+    """ Класс, который описывает узел связного списка. """
+
+    def __init__(self, value: Any, next_: Optional["Node"] = None):
+        """
+        Создаем новый узел для односвязного списка
+        :param value: Любое значение, которое помещено в узел
+        :param next_: следующий узел, если он есть
+        """
+        self.value = value
+
+        self.next = next_
+
+    def __repr__(self) -> str:
+        return f"Node({self.value}, {self.next})"
+
+    def is_valid(self, node: Any) -> None:
+        if not isinstance(node, (Node, type(None))):
+            raise TypeError
+
+    def set_next(self, next_: Optional["Node"] = None) -> None:
+        self.is_valid(next_)
+
+
+if __name__ == '__main__':
+    Node1 = Node(2, 2)
+
